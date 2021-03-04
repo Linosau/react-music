ee05cb777c6401ced2bc9e8d5a0c7ab08cda7dff
Linosau <liuf@liuf.com>
2021-03-04 14:51:10 +0800

    feat: 项目路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 import Discovery from '@/pages/discovery'
+import Recommend from '@/pages/discovery/c-pages/recommend'
+import Ranking from '@/pages/discovery/c-pages/ranking'
+import Songs from '@/pages/discovery/c-pages/songs'
+import Djradio from '@/pages/discovery/c-pages/djradio'
+import Artist from '@/pages/discovery/c-pages/artist'
+import Album from '@/pages/discovery/c-pages/album'
 import Mine from '@/pages/mine'
 import Friends from '@/pages/friends'
 
@@ -14,7 +20,40 @@ const routes = [
   },
   {
     path: '/discovery',
-    component: Discovery
+    component: Discovery,
+    routes: [
+      {
+        path: '/discovery',
+        exact: true,
+        render: () => (
+          <Redirect to={'/discovery/artist'} />
+        )
+      },
+      {
+        path: '/discovery/recommend',
+        component: Recommend
+      },
+      {
+        path: '/discovery/ranking',
+        component: Ranking
+      },
+      {
+        path: '/discovery/songs',
+        component: Songs
+      },
+      {
+        path: '/discovery/djradio',
+        component: Djradio
+      },
+      {
+        path: '/discovery/artist',
+        component: Artist
+      },
+      {
+        path: '/discovery/album',
+        component: Album
+      }
+    ]
   },
   {
     path: '/mine',
